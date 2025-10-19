d3c834c275a08b1a79a2178b0dde925bd43c7781
Kotoka Emmanuel <emmanuelyayrakotoka@gmail.com>
2025-10-19 12:36:35 +0000

    Work done

---
+++ alx_book_store.sql
@@ -0,0 +1,36 @@
+CREATE Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DECIMAL(10, 2),
+    published_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+CREATE Authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL,
+);
+
+CREATE Customers(
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE NOT NULL,
+    address TEXT
+);
+
+CREATE Orders(
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE Order_Details(
+    order_detail_id INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DECIMAL(10, 2),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
