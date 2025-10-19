bea7099a204ada3782ba2aaeb7e0ceaee6254554
Kotoka Emmanuel <emmanuelyayrakotoka@gmail.com>
2025-10-19 12:43:57 +0000

    Done

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
-CREATE Books (
+CREATE TABLE Books (
     book_id INT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
@@ -10,26 +10,26 @@ CREATE Books (
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
-CREATE Authors (
+CREATE TABLE Authors (
     author_id INT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL,
 );
 
-CREATE Customers(
+CREATE TABLE Customers(
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) UNIQUE NOT NULL,
     address TEXT
 );
 
-CREATE Orders(
+CREATE TABLE Orders(
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE Order_Details(
+CREATE TABLE Order_Details(
     order_detail_id INT PRIMARY KEY,
     order_id INT,
     book_id INT,
