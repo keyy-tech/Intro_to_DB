31532cda38ffe76202dfb46c21f76b28026e6a8e
Kotoka Emmanuel <emmanuelyayrakotoka@gmail.com>
2025-10-19 15:14:13 +0000

    Working

--- MySQLServer.py
+++ MySQLServer.py
@@ -11,7 +11,7 @@ def create_database():
         )
         if mydb.is_connected():
             query = mydb.cursor()
-            query.execute("CREATE DATABASE alx_book_store")
+            query.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully")
     except Error as e:
         print(f"Error is {e}")
