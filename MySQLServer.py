0ed9b9f8bc5397ca28e1944f135967f3482a4278
Kotoka Emmanuel <emmanuelyayrakotoka@gmail.com>
2025-10-19 15:15:05 +0000

    Working on it

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,5 +1,4 @@
 import mysql.connector
-from mysql.connector import Error
 
 
 def create_database():
@@ -13,7 +12,7 @@ def create_database():
             query = mydb.cursor()
             query.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully")
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error is {e}")
 
     finally:
